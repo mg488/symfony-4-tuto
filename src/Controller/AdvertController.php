13af4d790af9a152199b8fd199e99449f84e22c6
mg488 <contactvillagedenguith@gmail.com>
2020-06-05 06:17:34 +0200

    réorganisation menu

--- src/Controller/AdvertController.php
+++ src/Controller/AdvertController.php
@@ -11,7 +11,24 @@ class AdvertController extends AbstractController
     public function index()
     {
         return $this->render('advert/indexAdvert.html.twig', [
-            'controller_name' => 'AdvertController',
+            'name' => 'Mnd variable passee en parametre depuis le controlleur',
         ]);
+    }
+    public function view($id)
+    {
+        return $this->render('advert/viewAdvert.html.twig',['id'=>$id]);
+    }
+
+    public function add()
+    {
+        return $this->render('advert/addAdvert.html.twig');
+    }
+    public function edit($id)
+    {
+        return $this->render('advert/editAdvert.html.twig');
+    }
+    public function delete($id)
+    {
+        return $this->render('advert/deleteAdvert.html.twig');
     }
 }
