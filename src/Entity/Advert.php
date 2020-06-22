61d697a938399f6a1992ebe0fc248735595db2ff
mg488 <contactvillagedenguith@gmail.com>
2020-06-23 00:52:16 +0200

    validators

--- src/Entity/Advert.php
+++ src/Entity/Advert.php
@@ -78,7 +78,6 @@ class Advert
 
     /**
      * @ORM\Column(type="text")
-     * @Assert\Length(min=50, minMessage = "le contenu doit contenir au moins {{ limit }} caractères.")
      */
     private $content;
 
